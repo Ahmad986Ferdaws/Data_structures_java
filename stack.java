3316b7cc91a49394d31f548e53bd84400410c8d0
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-01-07 13:02:58 -0600

    Update stack.java

--- stack.java
+++ stack.java
@@ -2,3 +2,9 @@ import java.util.Stack;
 
  public class Main {
      public static  void main(String[] args) {
+ // we created declared the stack using the stack<string>
+         Stack<String> stack = new Stack<String>();
+
+
+        // with the ush item we can push the items
+         // think of it as we are adding new clothes to the stack of the clothes
