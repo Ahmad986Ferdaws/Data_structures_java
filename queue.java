58a22ae32adf717f3e2a0301e6cbe6e39bb828a3
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-01-08 15:19:49 -0600

    Update queue.java

--- queue.java
+++ queue.java
@@ -1,3 +1,6 @@
 import java.util.Queue;
 import java.util.LinkedList;
 
+// in this part of the lesson we are going to learn the queue //
+ // by the other words it means first serve first out like the way we do it with FIFO
+
