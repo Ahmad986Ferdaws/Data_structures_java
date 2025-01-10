da3afc47abd93ea824a346eff1672dbc4b4eeae3
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-01-10 11:49:31 -0600

    Update linked_lists.java

--- linked_lists.java
+++ linked_lists.java
@@ -1,1 +1,10 @@
 import java.util.LinkedList;
+
+////////////////////////////////////////////////////
+
+// in this part we will be studying the linked lists which are different that arrays so lets deep dive into the
+// easy comparison between a normal array list and the linked lists
+//  1-  in array list we can change the element by shifting so it will be very hard if we wanted the element one with
+// the element 1 million.
+// 2- but in this case linked lists are more beneficiary cause the linked lists have their own specific nodes connected
+// with the element.
