b63f90ec14a2e0b599d1a3c29dada6fe996a49da
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-01-13 23:49:16 -0600

    Update dynamic_arrays.java

--- dynamic_arrays.java
+++ dynamic_arrays.java
@@ -3,3 +3,4 @@ import java.util.*;
 // first  we look at the definition of the static array:
 
 // static array is fixed array, once we have declared it we cannot change the capacity of it
+// on the other hand dynamic arrays
