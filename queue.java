25a81c9d1748df09b444d255b76394616201fd9b
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-06-20 23:55:34 -0500

    Update queue.java

--- queue.java
+++ queue.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 // unlike the stack which we are using it for first in last out, it is directly the opposite of it
 
-public class queue {
+public class queue {aaa
 
     public static void main(String[]args){
 
