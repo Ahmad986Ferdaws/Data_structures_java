b6af3ae6b14197005eec14c11829ae5722052c81
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2025-06-24 23:30:07 -0500

    Update priority_queue.java

--- priority_queue.java
+++ priority_queue.java
@@ -3,7 +3,7 @@ import java.util.*;
 public static void main(String[] args) {
 
   Queue<Double> queue = new PriorityQueue<>();
-aaa
+aa
    queue.add(1.0);
      queue.add(2.0);
         queue.add(3.0);
